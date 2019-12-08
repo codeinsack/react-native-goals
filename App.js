222a36efc491c6166c36196ba253fd58cdba3e76
codeinsack <codeinsack@gmail.com>
2019-12-08 22:26:29 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput placeholder="Course Goal" style={{ borderColor: 'black', borderWidth: 1, padding: 10, width: '80%' }} />
+        <Button title="ADD" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
